2533a805fd2a7efe425dc6e2f952ac338ef039be
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-10-25 13:49:22 +0545

    minor adjustment for typescript

--- React Utility/ReactSetup/newSetUp.py
+++ React Utility/ReactSetup/newSetUp.py
@@ -2,10 +2,11 @@ import os
 
 # List of file name to remove .. mainly for js and typescript with tsx components, will treverse through both and delete accordingly
 fileNamesPublic=["favicon.ico", "logo192.png", "logo512.png"]
-fileNamesSrc=["App.test.js", "logo.svg", "reportWebVitals.js", "setupTests.js", "App.test.tsx", "reportWebVitals.tsx", "setupTests.tsx"]
-fileNamesSrcEmpty=["App.css", "index.css"]
+fileNamesSrc=["App.test.js", "logo.svg", "reportWebVitals.js", "setupTests.js", "App.test.tsx", "reportWebVitals.tsx", "setupTests.tsx", "react-app-env.d.ts"]
+fileNamesSrcEmpty=["App.css", "index.css",  "App.test.tsx"]
 fileNamesSrcJSModify=["index.js", "App.js", "index.tsx", "App.tsx"]
 
+# For js only, for tsx it is modified later..
 indexTemplateContent="import React from 'react';\nimport ReactDOM from 'react-dom/client';\nimport './index.css';\nimport App from './App';\n\nconst root = ReactDOM.createRoot(document.getElementById('root'));\nroot.render(\n\t<React.StrictMode>\n\t\t<App />\n\t</React.StrictMode>\n);"
 appTemplateContent="import './App.css';\n\nfunction App() {\n\treturn (\n\t\t<div className=\"App\">\n\n\t\t</div>\n\t);\n}\n\nexport default App;"
 
@@ -33,5 +34,10 @@ for file in fileNamesSrcEmpty:
 for index,file in enumerate(fileNamesSrcJSModify):
     if os.path.exists(currDir+"/src/"+file):
         with open(currDir+"/src/"+file,"w") as nf:
-            nf.write(modifyContent[index])
+            # For type script strict typechecking is required so...
+            if(file.count(".tsx")>0):
+                modifyContent[0] = "import React from 'react';\nimport ReactDOM from 'react-dom/client';\nimport './index.css';\nimport App from './App';\n\nconst root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);\nroot.render(\n\t<React.StrictMode>\n\t\t<App />\n\t</React.StrictMode>\n);"
+            nf.write(modifyContent[index%2])
+
+
 
